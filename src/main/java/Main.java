d41b2e90e93217ff662b5e275a60e6b800e877bb
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-03-22 23:54:08 +0000

    sorted hashmap by keys using a treemap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,23 +6,27 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         TextProcessor text = new TextProcessor(0, new HashMap<>(), 0, new ArrayList<>());
+
         File path = new File("C:\\Users\\Abdul\\Documents\\text.txt"); //PLEASE REPLACE WITH THE FILE PATH/DIRECTORY OF YOUR OWN TEXT FILE
+
         BufferedReader reader = new BufferedReader(new FileReader(path));
+
         String line;
-
         while((line = reader.readLine()) != null) {
-            String[] stringArr = line.split("[ .]+");
+            if (line.length() > 0) {
+                String[] stringArr = line.split("[ .]+");
 
-            Map<Integer, Integer> map = text.getWordLengths();
-            for (String word : stringArr) {
-                int length = word.length();
+                Map<Integer, Integer> map = text.getWordLengths();
+                for (String word : stringArr) {
+                    int length = word.length();
 
-                text.setWordCount(text.getWordCount() + 1);
+                    text.setWordCount(text.getWordCount() + 1);
 
-                if(map.containsKey(length)) {
-                    map.put(length, map.get(length) + 1);
-                } else {
-                    map.put(length, 1);
+                    if(map.containsKey(length)) {
+                        map.put(length, map.get(length) + 1);
+                    } else {
+                        map.put(length, 1);
+                    }
                 }
             }
         }
@@ -39,11 +43,13 @@ public class Main {
             }
         }
 
+        Map<Integer, Integer> sortedMap = new TreeMap<>(text.getWordLengths());
+
         DecimalFormat df = new DecimalFormat("0.000");
 
         System.out.println("Word count = " + text.getWordCount());
         System.out.println("Average word length = " + df.format(text.getAverageWordLength()));
-        for (Map.Entry<Integer, Integer> length : text.getWordLengths().entrySet()) {
+        for (Map.Entry<Integer, Integer> length : sortedMap.entrySet()) {
             System.out.println("Number of words of length " + length.getKey() + " is " + length.getValue());
         }
         System.out.println("The most frequently occurring word length is " + text.getCommonLengths() + ", for word lengths of " + lengths);
