c199bc7d248e6bcd07294276cd9665510a871a8f
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-03-22 19:45:58 +0000

    working version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,58 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        TextProcessor processor = new TextProcessor(0, new HashMap<>());
+        TextProcessor text = new TextProcessor(0, new HashMap<>(), 0, 0, new ArrayList<>());
         File path = new File("C:\\Users\\Abdul\\Documents\\test.txt");
-//        String path = "C:\\Users\\Abdul\\Documents\\test.txt";
         BufferedReader reader = new BufferedReader(new FileReader(path));
         String line;
-        while(reader.readLine() != null) {
-            line = reader.readLine();
-            String[] stringArr = line.split(" ");
+
+        while((line = reader.readLine()) != null) {
+            String[] stringArr = line.split("[ .]+");
+
+            Map<Integer, Integer> map = text.getLengthOccurrence();
             for (String word : stringArr) {
                 int length = word.length();
-                Map<Integer, Integer> map = processor.getLengthOccurrence();
 
-                processor.setWordCount(processor.getWordCount()+1);
+                text.setWordCount(text.getWordCount()+1);
 
-                if(map.get(length) == null) {
+                if(map.containsKey(length)) {
                     map.put(length, map.get(length) + 1);
+                } else {
+                    map.put(length, 1);
                 }
             }
 
         }
+
+        Integer highestOccurrence = Collections.max(text.getLengthOccurrence().values());
+        for (Map.Entry<Integer, Integer> entry : text.getLengthOccurrence().entrySet()) {
+            if(entry.getValue() == highestOccurrence) {
+                text.getMostCommonWords().add(entry.getKey());
+                text.setMostCommonLength(entry.getValue());
+            }
+        }
+
+        String lengths = "";
+
+        for (int i=0; i<text.getMostCommonWords().size(); i++) {
+            if(lengths == "") {
+                lengths += Integer.toString(text.getMostCommonWords().get(i));
+            } else {
+                lengths += " & " + text.getMostCommonWords().get(i);
+            }
+        }
+
+        DecimalFormat df = new DecimalFormat("0.000");
+
+        System.out.println("Word count = " + text.getWordCount());
+        System.out.println("Average word length = " + df.format(text.getAverage()));
+        for (Map.Entry<Integer, Integer> length : text.getLengthOccurrence().entrySet()) {
+            System.out.println("Number of words of length " + length.getKey() + " is " + length.getValue());
+        }
+        System.out.println("The most frequently occurring word length is " + text.getMostCommonLength() + ", for word lengths of " + lengths);
     }
 }
--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TextProcessor {
-    private int wordCount = 0;
+    private int wordCount;
     private Map<Integer, Integer> lengthOccurrence = new HashMap<>();
+    private Integer mostCommonLength;
+    private ArrayList<Integer> mostCommonWords = new ArrayList<>();
 
-    public TextProcessor(int wordCount, Map<Integer, Integer> lengthOccurrence) {
+    public TextProcessor(int wordCount, Map<Integer, Integer> lengthOccurrence, float average, Integer mostCommonLength, ArrayList<Integer> mostCommonWords) {
         this.wordCount = wordCount;
         this.lengthOccurrence = lengthOccurrence;
+        this.mostCommonLength = mostCommonLength;
+        this.mostCommonWords = mostCommonWords;
     }
 
     public TextProcessor() {
@@ -26,7 +32,28 @@ public class TextProcessor {
         return lengthOccurrence;
     }
 
-    public void setLengthOccurrence(Map<Integer, Integer> lengthOccurrence) {
-        this.lengthOccurrence = lengthOccurrence;
+    public float getAverage() {
+        float totalWordLength = 0;
+        float keyXValue = 0;
+
+        for (Integer key : getLengthOccurrence().keySet()) {
+            totalWordLength += getLengthOccurrence().get(key);
+            keyXValue += key * getLengthOccurrence().get(key);
+        }
+
+        float average = (keyXValue/totalWordLength);
+        return average;
+    }
+
+    public Integer getMostCommonLength() {
+        return mostCommonLength;
+    }
+
+    public void setMostCommonLength(Integer mostCommonLength) {
+        this.mostCommonLength = mostCommonLength;
+    }
+
+    public ArrayList<Integer> getMostCommonWords() {
+        return mostCommonWords;
     }
 }
